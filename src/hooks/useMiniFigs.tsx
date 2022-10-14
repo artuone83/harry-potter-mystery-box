aba779b041c581ca380369ada27664e25b467283
artur wozniak <artur.wozniak@grupawp.pl>
2022-10-14 21:48:39 +0200

    HP: fix duplicated imports

--- src/hooks/useMiniFigs.tsx
+++ src/hooks/useMiniFigs.tsx
@@ -1,7 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 
-import { getMiniFigs } from 'services/rebrickable/minifigs/get-minifigs';
-import { MiniFigsDTO } from 'services/rebrickable/minifigs/minifigs-dto';
+import { MiniFigsDTO, getMiniFigs } from 'services/rebrickable';
 
 export const useMiniFigs = (search: string) =>
   useQuery<MiniFigsDTO[]>(['minifigs', search], () => getMiniFigs(search));
