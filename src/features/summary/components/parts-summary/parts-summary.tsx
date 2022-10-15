b0ec8fc4c57b12e70fda334eb5be2c7a77c3d2c7
artur wozniak <artur.wozniak@grupawp.pl>
2022-10-15 20:30:06 +0200

    refactor: parts summary is loading state

--- src/features/summary/components/parts-summary/parts-summary.tsx
+++ src/features/summary/components/parts-summary/parts-summary.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, Button, Stack, Typography } from '@mui/material';
 import React from 'react';
 
 import { Loading } from 'components';
@@ -11,33 +11,39 @@ export const PartsSummary = () => {
   const { data: details, isLoading: areDetailsLoading } = useMinifigDetailsQuery(set_num as string);
   const { data: parts, isLoading: arePartsLoading } = useMinifigPartsQuery(set_num as string);
 
+  const allDataLoaded = parts && details;
+
   return (
     <Section sx={{ border: '6px solid lightSalmon' }}>
       {(areDetailsLoading || arePartsLoading) && <Loading />}
-      <Typography component="h2" sx={{ marginBottom: 5, border: '12px solid lightGreen' }}>
-        SUMMARY
-      </Typography>
-      {details && (
-        <Box sx={{ marginBottom: 5, border: '12px solid lightGreen' }}>
-          <Typography>{details.set_img_url}</Typography>
-          <Typography>{details.name}</Typography>
-        </Box>
-      )}
-      {parts && (
-        <Box sx={{ border: '12px solid lightBlue' }}>
-          <Typography component="h3">There are {parts.count} parts in this minifig:</Typography>
-          <Stack direction="column" spacing={2}>
-            {parts.results.map(({ part: { part_img_url, name, part_num } }) => (
-              <Stack key={name} direction="row" spacing={3} alignItems="center">
-                <p>{part_img_url}</p>
-                <Stack direction="column" spacing={4} justifyContent="center">
-                  <Typography>{name}</Typography>
-                  <Typography sx={{ color: 'orange' }}>{part_num}</Typography>
+      {allDataLoaded && (
+        <>
+          <Typography component="h2" sx={{ marginBottom: 5, border: '12px solid lightGreen' }}>
+            SUMMARY
+          </Typography>
+
+          <Box sx={{ marginBottom: 5, border: '12px solid lightGreen' }}>
+            <Typography>{details.set_img_url}</Typography>
+            <Typography>{details.name}</Typography>
+          </Box>
+
+          <Box sx={{ border: '12px solid lightBlue' }}>
+            <Typography component="h3">There are {parts.count} parts in this minifig:</Typography>
+            <Stack direction="column" spacing={2}>
+              {parts.results.map(({ part: { part_img_url, name, part_num } }) => (
+                <Stack key={name} direction="row" spacing={3} alignItems="center">
+                  <p>{part_img_url}</p>
+                  <Stack direction="column" spacing={4} justifyContent="center">
+                    <Typography>{name}</Typography>
+                    <Typography sx={{ color: 'orange' }}>{part_num}</Typography>
+                  </Stack>
                 </Stack>
-              </Stack>
-            ))}
-          </Stack>
-        </Box>
+              ))}
+            </Stack>
+          </Box>
+
+          <Button variant="contained">Submit</Button>
+        </>
       )}
     </Section>
   );
